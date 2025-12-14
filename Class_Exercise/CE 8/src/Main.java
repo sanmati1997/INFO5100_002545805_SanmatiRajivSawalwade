9173bec7699384960cb0a83a8f22758b02f26b24
sanmati1997 <sanmati.sawalwade1231@gmail.com>
2025-12-13 18:20:29 -0800

    adding json lib

--- Class_Exercise/CE 8/src/Main.java
+++ Class_Exercise/CE 8/src/Main.java
@@ -4,6 +4,15 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+
+//# How to add org.json library (without Maven)
+//        * Download the JSON library JAR file (included under lib dir locally along with my project):
+//        https://repo1.maven.org/maven2/org/json/json/20231013/json-20231013.jar
+//        * Open IntelliJ → File → Project Structure
+//        * Click "Project Settings" and click "library"
+//        * Click the + button → choose "JARs or Directories"
+//        * Select the path for json-20231013.jar file (under lib dir locally in my submitted project)
+//        * Click "Apply"
 import org.json.JSONArray;
 import org.json.JSONObject;
 
